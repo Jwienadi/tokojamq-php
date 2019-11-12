ff4e0f86329da677de4506b798eb82ca4e851814
Jessicawie <jwienadi@gmail.com>
2019-11-12 13:14:43 +0700

    update config to cloud db

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
     $base_url = "http://localhost/tokojamq/tokojamq-php/";
-    $servername = "localhost";
-    $username 	= "root";
-    $password 	= "";
-    $dbname 	= "tokojamq";
+    $servername = "jmswijaya.com";
+    $username 	= "isb18";
+    $password 	= "Isb_2018";
+    $dbname 	= "store18_1";
 
     $con = new mysqli($servername, $username, $password,$dbname);
 
