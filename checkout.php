cb858cb64d01a6c9b09e452f542025eb71f85ee6
Melisa Krisnawati <mkrisnawati@student.ciputra.ac.id>
2019-12-01 22:50:59 +0700

    a

--- checkout.php
+++ checkout.php
@@ -23,7 +23,7 @@ require_once('config.php');
   <link href="assets/css/simple-sidebar.css" rel="stylesheet">
   <link href="assets/css/shop-homepage.css" rel="stylesheet">
   <link href="assets/css/headerfooter.css" rel="stylesheet">
-<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/checkout/">
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/checkout/">
 
     <!-- Custom styles for this template -->
     <link href="form-validation.css" rel="stylesheet">
@@ -117,65 +117,83 @@ require_once('config.php');
             </nav>
   
     <!-- CHECKOUT COY -->
-    <div class="container">
-    <h1> Checkout Form </h1>
-    <div class="col-md-4 order-md-2 mb-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">Your cart</span>
-            <span class="badge badge-secondary badge-pill">3</span>
-          </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
-
-          <form class="card p-2">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Promo code">
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-secondary">Redeem</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-      <!--<div class="py-5 text-center">
-        <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h2>Checkout form</h2>
-        <p class="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
-      </div>-->
-
-      <div class="row">
+    <main id="main" role="main">
+        <section id="checkout-container">
+          <div class="container">
+            <h1> Toko JamQ Checkout Form </h1>
+                <div class="row py-2">
+                    <div class="col-md-4 order-md-2 mb-4">
+                        <h4 class="d-flex justify-content-between align-items-center mb-3">
+                            <span class="text-muted">Your cart</span>
+                            <span class="badge badge-secondary badge-pill">3</span>
+                        </h4>
+                        <ul class="list-group mb-3">
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">Product name</h6>
+                                    <small class="text-muted">Brief description</small>
+                                </div>
+                                <span class="text-muted">$12</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">Second product</h6>
+                                    <small class="text-muted">Brief description</small>
+                                </div>
+                                <span class="text-muted">$8</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">Third item</h6>
+                                    <small class="text-muted">Brief description</small>
+                                </div>
+                                <span class="text-muted">$5</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between bg-light">
+                                <div class="text-success">
+                                    <h6 class="my-0">Promo code</h6>
+                                    <small>EXAMPLE CODE</small>
+                                </div>
+                                <span class="text-success">-$5</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between">
+                                <span>Total (USD)</span>
+                                <strong>$20</strong>
+                            </li>
+                        </ul>
+                        <form class="card p-2">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Promo code">
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-secondary">Redeem</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="payment-methods">
+                            <p class="pt-4 mb-2">Payment Options</p>
+                            <hr>
+                            <ul class="list-inline d-flex">
+                                <li class="mx-1 text-info">
+                                    <i class="fa-2x fa fa-cc-visa"></i>
+                                </li>
+                                <li class="mx-1 text-info">
+                                    <i class="fa-2x fa fa-cc-stripe"></i>
+                                </li>
+                                <li class="mx-1 text-info">
+                                    <i class="fa-2x fa fa-cc-paypal"></i>
+                                </li>
+                                <li class="mx-1 text-info">
+                                    <i class="fa-2x fa fa-cc-jcb"></i>
+                                </li>
+                                <li class="mx-1 text-info">
+                                    <i class="fa-2x fa fa-cc-discover"></i>
+                                </li>
+                                <li class="mx-1 text-info">
+                                    <i class="fa-2x fa fa-cc-amex"></i>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
         <div class="col-md-8 order-md-1">
           <h4 class="mb-3">Billing address</h4>
           <form class="needs-validation" novalidate>
@@ -210,7 +228,7 @@ require_once('config.php');
             </div> -->
 
             <div class="mb-3">
-              <label for="email">Email <span class="text-muted">(Optional)</span></label>
+              <label for="email">Email <span class="text-muted"></span></label>
               <input type="email" class="form-control" id="email" placeholder="you@example.com">
               <div class="invalid-feedback">
                 Please enter a valid email address for shipping updates.
@@ -219,7 +237,7 @@ require_once('config.php');
 
             <div class="mb-3">
               <label for="address">Address</label>
-              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+              <input type="text" class="form-control" id="address" placeholder="Jalan blablabla" required>
               <div class="invalid-feedback">
                 Please enter your shipping address.
               </div>
@@ -235,20 +253,20 @@ require_once('config.php');
                 <label for="country">Country</label>
                 <select class="custom-select d-block w-100" id="country" required>
                   <option value="">Choose...</option>
-                  <option>United States</option>
+                  <option>Indonesia</option>
                 </select>
                 <div class="invalid-feedback">
                   Please select a valid country.
                 </div>
               </div>
               <div class="col-md-4 mb-3">
-                <label for="state">State/Province</label>
+                <label for="state">Province</label>
                 <select class="custom-select d-block w-100" id="state" required>
                   <option value="">Choose...</option>
-                  <option>California</option>
+                  <option>East Java</option>
                 </select>
                 <div class="invalid-feedback">
-                  Please provide a valid state/province.
+                  Please provide a valid province.
                 </div>
               </div>
               <div class="col-md-5 mb-3">
@@ -268,14 +286,15 @@ require_once('config.php');
                   Postal code required.
                 </div>
               </div>
-            </div>
-            <div class="col-md-6 mb-3">
-                <label for="zip">Recipient Mobile Phone</label>
-                <input type="text" class="form-control" id="zip" placeholder="" required>
+              <div class="col-md-5 mb-3">
+                <label for="phone">Recipient Mobile Phone</label>
+                <input type="text" class="form-control" id="phone" placeholder="" required>
                 <div class="invalid-feedback">
                 Recipient Mobile Phone required.
             </div>
             </div>
+            </div>
+
             <hr class="mb-4">
             <div class="custom-control custom-checkbox">
               <input type="checkbox" class="custom-control-input" id="same-address">
