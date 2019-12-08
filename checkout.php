dc5022588b18f3a3e7cf57a9b4f83ad680bf5b0b
Melisa Krisnawati <mkrisnawati@student.ciputra.ac.id>
2019-12-08 23:50:04 +0700

    aaaa

--- checkout.php
+++ checkout.php
@@ -4,8 +4,8 @@ require_once('config.php');
 include ('API/API.php');
 ?>
 <?php
-$query="select first_name,last_name,email"
-
+$query="select first_name,last_name,email";
+$qpromo="SELECT kode_promo, jumlah_promo, sistem_promo FROM promo_code p";
 ?>
 
 <!doctype html>
@@ -158,7 +158,7 @@ $query="select first_name,last_name,email"
                       <h6 class="my-0">Promo code</h6>
                       <small>EXAMPLE CODE</small>
                     </div>
-                    <span class="text-success">-$5</span>
+                    <span class="text-success">-Rp<?php echo $qpromo['jumlah_promo']; ?></span>
                   </li>
                   <li class="list-group-item d-flex justify-content-between">
                     <span>Total (USD)</span>
@@ -518,6 +518,42 @@ $query="select first_name,last_name,email"
             })
     })
     });
+
+    // buat promo code woyy
+    $("#apply").click(function(){
+		if($('#promo_code').val()!=''){
+			$.ajax({
+						type: "POST",
+						url: "process.php",
+						data:{
+							coupon_code: $('#coupon_code').val()
+						},
+						success: function(dataResult){
+							var dataResult = JSON.parse(dataResult);
+							if(dataResult.statusCode==200){
+								var after_apply=$('#total_price').val()-dataResult.value;
+								$('#total_price').val(after_apply);
+								$('#apply').hide();
+								$('#edit').show();
+								$('#message').html("Promocode applied successfully !");
+								
+							}
+							else if(dataResult.statusCode==201){
+								$('#message').html("Invalid promocode !");
+							}
+						}
+			});
+		}
+		else{
+			$('#message').html("Promocode can not be blank .Enter a Valid Promocode !");
+		}
+	});
+	$("#edit").click(function(){
+		$('#coupon_code').val("");
+		$('#apply').show();
+		$('#edit').hide();
+		location.reload();
+	});
     /*
      function ajaxfunction{
       var cat_id=document.getElementById('state').value;
