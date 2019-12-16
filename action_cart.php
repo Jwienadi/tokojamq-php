c2b8c32d48e4b35e228512b129b634ddc1166460
Jessicawie <jwienadi@gmail.com>
2019-12-16 23:56:12 +0700

    Update action_cart.php

--- action_cart.php
+++ action_cart.php
@@ -1,11 +1,12 @@
 <?php
+session_start();
 require_once('config.php');
 //if (isset($_POST)){
   if(isset($_GET['id'])){
     $product_id = $_GET['id'];
     if(!isset($_SESSION['user_id'])){
     header("Location: index.php");
-    } else {
+        } else {
   
    $cmd = "SELECT concat(m.nama_merk,' ',p.nama_product) as 'judul_barang',w.warna,p.harga_jual as 'harga_barang' ,bp.jumlah_barang, bp.product_warna_id as 'id' ,bp.id_barang_penjualan 
    from barang_penjualan bp, transaksi_penjualan tp, product_warna pw, product p, warna w, merk m 
@@ -14,7 +15,7 @@ require_once('config.php');
    //$cektransaksi= "SELECT * FROM transaksi_penjualan t where id_transaksi_penjualan='$_GET[id_transaksi_penjualan]'"
    $all_result 	= mysqli_query($con,$cmd) or die(mysqli_error($con));
    $ketemu = mysqli_num_rows($all_result);
-  
+  //echo $ketemu;
    $products = null;
    if ($ketemu >= 1){
        while($row = mysqli_fetch_assoc($all_result)) {
@@ -33,6 +34,7 @@ require_once('config.php');
      //if (isset($_GET['id'])){
       $sql="INSERT INTO barang_penjualan(id_transaksi_penjualan,`status`,product_warna_id,jumlah_barang)
       values (1, 0, '".$_GET['id']."' ,".$_GET['qty'].");";
+      echo $sql;
       $createsql=mysqli_query($con,$sql) or die(mysqli_error($con));
      // }
      // $tmp_cmd = "INSERT INTO barang_penjualan(id_barang_penjualan,id_transaksi_penjualan,`status`,product_warna_id,jumlah_barang)
@@ -42,10 +44,10 @@ require_once('config.php');
      // die();
   } else {
       //  kalau barang ada, maka di jalankan perintah update
-    
-      mysqli_query($con, "UPDATE barang_penjualan
-              SET jumlah_barang = jumlah_barang + '$_GET['qty']'
-              WHERE id_barang_penjualan ='$id_bp' AND product_warna_id='$_GET['id']'");       
+    /*$queryupdate="UPDATE barang_penjualan
+    SET jumlah_barang=jumlah_barang + '$_GET['qty']'
+    WHERE id_barang_penjualan ='$id_bp' AND product_warna_id='$_GET['id']'"
+      mysqli_query($con,$queryupdate);       */
   }   
   header('Location: cart.php');
    $_SESSION['cart']=$products;
