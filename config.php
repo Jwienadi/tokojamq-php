cc9939d9e1db0fc2afab1e48b8b0e7f0fdb9bf1f
Jessica Wienadi <50088810+Jwienadi@users.noreply.github.com>
2023-03-09 13:44:29 +0700

    Update config.php
    hide confidential datas

--- config.php
+++ config.php
@@ -1,23 +1,18 @@
 <?php
-    /*$base_url = "http://localhost/tokojamq/tokojamq-php/";
-    $servername = "jmswijaya.com";
-    $username 	= "isb18";
-    $password 	= "Isb_2018";
-    $dbname 	= "store18_1";*/
     $ip_server = $_SERVER['SERVER_ADDR'];
 
     if(($ip_server=='127.0.0.1')||($ip_server=="::1")){
         $base_url = "http://localhost/tokojamq/tokojamq-php/";
-        $servername = "jmswijaya.com";
-        $username 	= "isb18";
-        $password 	= "Isb_2018";
-        $dbname 	= "store18_1";
+        $servername = "hidden";
+        $username 	= "hidden";
+        $password 	= "hidden";
+        $dbname 	= "hidden";
     } else {
         $base_url = "http://tokojamq.xyz/";
-        $servername = "jmswijaya.com";
-        $username 	= "isb18";
-        $password 	= "Isb_2018";
-        $dbname 	= "store18_1";
+        $servername = "hidden";
+        $username 	= "hidden";
+        $password 	= "hidden";
+        $dbname 	= "hidden";
     }
 
     $con = new mysqli($servername, $username, $password,$dbname);
